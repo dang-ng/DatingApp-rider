23e1ddaa5babcea2f1dcbfe4dc2b220ba371459a
derek089 <nguyen.hai.dang@icloud.com>
2022-12-20 20:12:29 +0700

    update

---
+++ DatingApp/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class UsersController
+{
+    public UsersController()
+    {
+        
+    }
+}
