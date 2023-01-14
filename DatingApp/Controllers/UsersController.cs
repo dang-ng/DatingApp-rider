0c324a4dc8ab90630f4341280ba28177a06d4bd4
derek089 <nguyen.hai.dang@icloud.com>
2023-01-14 19:27:42 +0700

    angular added

--- DatingApp/Controllers/UsersController.cs
+++ DatingApp/Controllers/UsersController.cs
@@ -1,14 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
 
-public class UsersController
+public class UsersController : ControllerBase
 {
-    public UsersController()
+    private readonly DataContext context;
+    public UsersController(DataContext context)
     {
-        
+        this.context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        return await context.Users.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await context.Users.FindAsync(id);
     }
 }
